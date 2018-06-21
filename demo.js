35182ecacdfd3675b896e9c5762c16c8a15eb66f
rutujamalusare <mrutuja24.rm@gmail.com>
2018-06-21 12:25:40 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,1 @@
+alert('This is external js file');
---
+++ js/js.js
@@ -0,0 +1,22 @@
+console.log(`Welcome to codekul`)
+
+var nm = 'android'
+nm = 56.4
+let num = 10
+
+if(true) {
+let num = 20
+}
+console.log(`Number is ${num}`)
+
+function age() {
+    return num
+}
+
+function walk(stps){
+console.log(`you walked ${stps}`)
+}
+
+function run(stps) {
+    return stps * stps
+}
