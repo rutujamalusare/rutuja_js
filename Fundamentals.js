bbe4e8079ffdd0e5d9a5c42751f0cf88f2e11cc4
rutujamalusare <39490623+rutujamalusare@users.noreply.github.com>
2018-08-07 12:14:42 +0530

    Add files via upload

---
+++ Fundamentals.js
@@ -0,0 +1,13 @@
+let obj = {
+    nm : 'Android',
+    age : 10,
+    calcVer : function() {
+        return 10
+    }
+}
+
+console.log(`
+    Name is ${obj.nm} 
+    age is ${obj.age} 
+    version is ${obj.calcVer()}
+`)
---
+++ helloWorld.js
@@ -0,0 +1,17 @@
+console.log('hello World')
+
+var nm= 'Android'
+nm= 45
+
+function whatsNm(){
+    console.log('hello')
+}
+
+whatsNm()
+
+
+let cntr = 0
+while(true) {
+    if(cntr >= nms.length) break
+    console.log(`value is ${nms[cntr]}`)
+}
